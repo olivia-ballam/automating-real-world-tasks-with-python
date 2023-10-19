e7f8507edf4caee889ff6c9af536ab34944abe48
Olivia Ballam <oliviaballamart@gmail.com>
2023-10-19 18:45:57 +1000

    debugging and testing

--- week1-manipulating-images/scale_convert_images_using_pil.py
+++ week1-manipulating-images/scale_convert_images_using_pil.py
@@ -15,23 +15,21 @@ def rotate_scale_convert_images(path, rotate, resize, format, save_path):
         save_path (str): Path to save the converted images. 
     """
     try:
-        # List images in specified path
+        
         directory = os.listdir(path)
         for image in directory:
-            # Loop through images in directory
-            if image.endswith(".tiff"):
-                name = image.split(".")[0]
-                with Image.open(os.path.join(path, image)) as im:
-                    # Rotate, resize, and save the image to save_path with specified file format. 
-                    im.rotate(rotate).resize(resize).save(f"{save_path}{name}.{format}")
+            if image != ".DS_Store":
+                im = Image.open(os.path.join(path, image).convert("RGB"))
+                im.rotate(rotate).resize(resize).convert("RGB").save("{0}{1}".format(path, image.split(".")[0]), format)
+
     except Exception as e:
         print(f"ERROR {e}")
-#path = "images/"
-#save_path = "/opt/icons/"
-path = "/Users/oball/automating-real-world-tasks-with-python/week1-manipulating-images/testing_images"
-save_path = "/Users/oball/automating-real-world-tasks-with-python/week1-manipulating-images"
-rotate, resize, format = [90, (128, 128), "jpeg"]
-rotate_scale_convert_images(path, rotate, resize, format, save_path)
+
+path = "/images"
+save_path = "/opt/icons/"
+rotate, resize, format = [90, (128, 128), "JPEG"]
+rotate_scale_convert_images(path, rotate, resize, format, path)
 
 
 
+# TODO ERROR cannot write mode LA as JPEG
