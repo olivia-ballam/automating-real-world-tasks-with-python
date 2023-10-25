8cbf18b59ca5b3b7bca472803975da991a37e27e
Olivia Ballam <oliviaballamart@gmail.com>
2023-10-25 14:13:40 +1000

    process_text_files function commenting update

--- week2-interacting-with-web-services/process_text_files.py
+++ week2-interacting-with-web-services/process_text_files.py
@@ -4,19 +4,20 @@ import os
 import requests
 
 def send_feedback_request(text_file, ip_address):
+    """Send feedback request to server."""
 
-
-    # open and read text file 
+    # open and read text file. 
     with open(text_file, 'r') as f:
         title = f.readline().strip()
         name = f.readline().strip()
         date = f.readline().strip()
         review = f.read().strip()
 
-        # text file realine dictionary
+        # Convert text file to dictionary.
         feedback = {"title": title, "name": name, "date": date, "feedback": review}
 
         try:
+            # Send request to server. 
             response = requests.post(f"http://{ip_address}/feedback/", data=feedback)
         except Exception as e:
             print(f"ERROR: {e} unable to connect to server")
@@ -26,10 +27,8 @@ if __name__ == "__main__":
 
     path = "/data/feedback"
     text_files = os.listdir(path)
-
     ip_adress = "35.188.67.234"
 
     for text in text_files:
-
         send_feedback_request(text, ip_adress)
 
